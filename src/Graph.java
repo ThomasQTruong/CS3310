8b88617d77891f2e5d1211a8a9b203c2b0c64d5c
Thomas Truong <TruongThomasQ@gmail.com>
2024-02-29 04:19:04 -0800

    Set up some of the basic graph structure.

--- src/Graph.java
+++ src/Graph.java
@@ -1,8 +1,39 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
+ * <p>
+ * Thomas Truong
+ * Bronco ID: 014426906
+ * CS 3310, Spring 2024
+ * Programming Assignment 1
+ *
  * Graph.java
+ * Implementation for a Graph data structure.
  *
  * Copyright (c) 2024, Thomas Truong.
+ * </p>
  */
-
 public class Graph {
+  private Map<Integer, List<Integer>> adjacencyList;
+
+  /**
+   * Default Constructor: create a hashmap as the adjacency list.
+   */
+  public Graph() {
+    adjacencyList = new HashMap<Integer, List<Integer>>();
+  }
+
+  public void addVertex() {
+  }
+
+  public void addEdge() {
+  }
+
+  public void removeVertex() {
+  }
+
+  public void removeEdge() {
+  }
 }
