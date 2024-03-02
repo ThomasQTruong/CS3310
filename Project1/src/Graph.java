208e4ac3e7d8614cc3283bad421b2a1f2dfce9f2
Thomas Truong <TruongThomasQ@gmail.com>
2024-03-01 21:50:13 -0800

    Added file reader to create graphs.

--- Project1/src/Graph.java
+++ Project1/src/Graph.java
@@ -11,7 +11,7 @@ import java.util.Map;
  * Programming Assignment 1
  *
  * Graph.java
- * Implementation for a Graph data structure.
+ * Simple implementation for a Graph data structure.
  *
  * Copyright (c) 2024, Thomas Truong.
  * </p>
@@ -20,22 +20,73 @@ public class Graph {
   private Map<Integer, List<Integer>> adjacencyList;
 
   /**
-   * Default Constructor: create a hashmap as the adjacency list.
+   * Default Constructor: initialize a hashmap as the adjacency list.
    */
   public Graph() {
+    // Initialize list.
     adjacencyList = new HashMap<Integer, List<Integer>>();
   }
 
-  public void addVertex(int value) {
+
+  /**
+   * Constructor: create a graph with a specified amount of verticies. 
+   *
+   * @param amount - the amount of verticies to create.
+   */
+  public Graph(int amount) {
+    // Initialize list.
+    adjacencyList = new HashMap<Integer, List<Integer>>();
+
+    // Create the specified amount of verticies.
+    for (int i = 1; i <= amount; ++i) {
+      adjacencyList.put(i, new LinkedList<Integer>());
+    }
+  }
+
+
+  /**
+   * Adds a vertex to the graph.
+   *
+   * @param value - the value of the vertex.
+   * @return boolean - whether the operation was successful or not.
+   */
+  public boolean addVertex(int value) {
+    // If the vertex already exists.
+    if (adjacencyList.containsKey(value)) {
+      return false;
+    }
+
+    // Vertex is unique, create.
     adjacencyList.put(value, new LinkedList<Integer>());
+    return true;
   }
 
-  public void addEdge() {
-  }
 
-  public void removeVertex() {
-  }
+  /**
+   * Adds an edge which connects two verticies together.
+   *
+   * @param vertex1 - the first vertex.
+   * @param vertex2 - the second vertex.
+   * @return boolean - whether the operation was successful or not.
+   */
+  public boolean addEdge(int vertex1, int vertex2) {
+    // If the verticies do not exist, return false.
+    if (adjacencyList.containsKey(vertex1) == false) {
+      return false;
+    }
+    if (adjacencyList.containsKey(vertex2) == false) {
+      return false;
+    }
 
-  public void removeEdge() {
+    // Since both exist, check if linked already.
+    if (adjacencyList.get(vertex1).contains(vertex2)) {
+      return false;
+    }
+
+    // Link the two verticies.
+    adjacencyList.get(vertex1).add(vertex2);
+    adjacencyList.get(vertex2).add(vertex1);
+
+    return true;
   }
 }
---
+++ Project1/src/GraphTester.java
@@ -0,0 +1,91 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * <p>
+ * Thomas Truong
+ * Bronco ID: 014426906
+ * CS 3310, Spring 2024
+ * Programming Assignment 1
+ * 
+ * GraphTester.java
+ * Driver class to test out graph data structures and 
+ * BFS algorithm for Graph.java.
+ *
+ * Copyright (c) 2024, Thomas Truong.
+ * </p>
+ */
+public class GraphTester {
+  private static ArrayList<Graph> graphList = new ArrayList<Graph>();
+
+  /**
+   * Converts data from a file into graph(s).
+   * Prints and exits if file does not exist.
+   *
+   * @param fileName - the name of the file.
+   */
+  public static void fileToGraphs(String fileName) {
+    try {
+      File inputFile = new File(fileName);
+      Scanner fileReader = new Scanner(inputFile);
+
+      // For every line, create graph from input.
+      while (fileReader.hasNextLine()) {
+        String data = fileReader.nextLine();
+        graphList.add(inputLineToGraph(data));
+      }
+
+      fileReader.close();
+    } catch (FileNotFoundException e) {
+      // File was unreadable.
+      System.out.println("Invalid file: " + fileName + ".");
+      System.exit(1);
+    }
+  }
+
+
+  /**
+   * Creates a graph from the input line.
+   *
+   * @param inputLine - the line from the input file.
+   * @return Graph - the converted graph.
+   */
+  public static Graph inputLineToGraph(String inputLine) {
+    // Get rid of spaces and ): "7 (1,2) (3,4)" => "7(1,2(3,4".
+    String parsed = inputLine.replace(" ", "");
+    parsed = parsed.replace(")", "");
+
+    // "7(1,2(3,4" => {"7", "1,2", "3,4"}.
+    String[] values = parsed.split("\\(");
+
+    // Create graph with the inputted amount of verticies.
+    Graph graph = new Graph(Integer.valueOf(values[0]));
+
+    // Add every edge.
+    for (int i = 1; i < values.length; ++i) {
+      // Extract verticies from the values.
+      String[] verticies = values[i].split(",");
+
+      graph.addEdge(Integer.valueOf(verticies[0]), Integer.valueOf(verticies[1]));
+    }
+
+    return new Graph();
+  }
+
+
+  /**
+   * The start of the program. 
+   *
+   * @param args - command-line arguments (file name).
+   */
+  public static void main(String[] args) {
+    fileToGraphs(args[0]);
+
+    for (int i = 0; i < graphList.size(); ++i) {
+      System.out.println(graphList.get(i));
+    }
+  }
+}
+
