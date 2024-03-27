2467861e2f809985f165f5e6dec83e33b2aded96
Thomas Truong <TruongThomasQ@gmail.com>
2024-03-26 17:09:41 -0700

    Created basic structure for Project2.

---
+++ Project2/AnagramDetector.java
@@ -0,0 +1,18 @@
+/**
+ * <p>
+ * * Thomas Truong
+ * Bronco ID: 014426906
+ * CS 3310, Spring 2024
+ * Programming Assignment 2
+ * 
+ * AnagramDetector.java
+ * Detects and creates a set of all anagrams in a data file.
+ *
+ * Copyright (c) 2024, Thomas Truong
+ * </p>
+ */
+public class AnagramDetector {
+  public static void main(String[] args) {
+    return;
+  }
+}
