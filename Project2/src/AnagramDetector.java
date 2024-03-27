a41dd4f1f0b3c6de9dd5031f2a1f47b76e3c44dd
Thomas Truong <TruongThomasQ@gmail.com>
2024-03-26 17:41:50 -0700

    Created extractData(); fixed make run.

--- Project2/src/AnagramDetector.java
+++ Project2/src/AnagramDetector.java
@@ -1,3 +1,10 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 /**
  * <p>
  * * Thomas Truong
@@ -12,8 +19,49 @@
  * </p>
  */
 public class AnagramDetector {
+  /**
+   * Extracts data from a file into an ArrayList.
+   *
+   * @param fileName - the name of the data file.
+   * @return ArrayList[String] - the list of words from the data file.
+   */
+  public static ArrayList<String> extractData(String fileName) {
+    // List that contains the words from the data file.
+    ArrayList<String> words = new ArrayList<String>();
+
+    // Try to read file.
+    try {
+      File data = new File(fileName);
+      Scanner fileReader = new Scanner(data);
+
+      // Add every word into the list.
+      while (fileReader.hasNextLine()) {
+        words.add(fileReader.nextLine());
+      }
+
+      // Finished reading from file, close Scanner.
+      fileReader.close();
+    } catch (FileNotFoundException e) {
+      // Something went wrong.
+      System.out.println("Unable to extract from data file: " + fileName + ".");
+      System.exit(1);
+    }
+
+    return words;
+  }
+
+  /**
+   * Gets the sets of anagrams of a data file.
+   *
+   * @param words - the list of words from the data file. 
+   * @return - HashSet[ArrayList[String]] the set of every anagram.
+   */
+  public static HashSet<ArrayList<String>> getAnagramsSet(ArrayList<String> words) {
+    return new HashSet<ArrayList<String>>();
+  }
+
   public static void main(String[] args) {
-    System.out.println(args[0]);
+    System.out.println(extractData(args[0]));
 
     return;
   }
