eed58af1085384f74e8e6c9aa36d7233ec231b69
Thomas Truong <TruongThomasQ@gmail.com>
2024-04-24 23:50:38 -0700

    Created the basic file structure.

---
+++ Project3/README.md
@@ -0,0 +1,3 @@
+# [CS 3310] Programming Assignment 3 - Canoe Rental Pathing
+By: Thomas Truong\
+Bronco ID: 014426906
---
+++ Project3/src/CanoeRentalPathing.java
@@ -0,0 +1,17 @@
+/**
+ * <p>
+ * Thomas Truong
+ * Bronco ID: 014426906
+ * CS 3310, Spring 2024
+ * Programming Assignment 3
+ * 
+ * CanoeRentalPathing.java
+ * Computes the most optimal path for a canoe trip.
+ * 
+ * Copyright (c) 2024, Thomas Truong.
+ * </p>
+ */
+public class CanoeRentalPathing {
+  public static void main(String[] args) {
+  }
+}
